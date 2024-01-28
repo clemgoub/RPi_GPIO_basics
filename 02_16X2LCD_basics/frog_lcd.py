dbf69f9263da86c7ec0b5b297ac719eaca18e048
Clement Goubert <goubert.clement@gmail.com>
2024-01-28 14:46:44 -0500

    frog 1

---
+++ 02_16X2LCD_basics/frog_lcd.py
@@ -0,0 +1,64 @@
+from RPLCD.i2c import CharLCD
+import time
+
+# create lcd object with write address.
+lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1, cols=16, rows=2, dotsize=8)
+lcd.clear()
+
+# define sleep time for tuning
+st = 0.2
+# Animation
+for i in range(20):
+	# frame 1
+	lcd.write_string('      **      ')
+	lcd.cursor_pos = (1,0)
+	lcd.write_string('  (--------)  ')
+	time.sleep(st)
+	lcd.clear()
+	# frame 2
+	lcd.write_string('      **      ')
+	lcd.cursor_pos = (1,0)
+	lcd.write_string(' (----------) ')
+	time.sleep(st)
+	lcd.clear()
+	# frame 3 (same as 1)
+	lcd.write_string('      **      ')
+	lcd.cursor_pos = (1,0)
+	lcd.write_string('  (--------)  ')
+	time.sleep(st)
+	lcd.clear()
+	# frame 4
+	lcd.write_string('      **      ')
+	lcd.cursor_pos = (1,0)
+	lcd.write_string('   (------)   ')
+	time.sleep(st)
+	lcd.clear()
+	# frame 5
+	lcd.write_string('      **      ')
+	lcd.cursor_pos = (1,0)
+	lcd.write_string('    (----)    ')
+	time.sleep(st)
+	lcd.clear()
+	# frame 6 (4)
+	lcd.write_string('      **      ')
+	lcd.cursor_pos = (1,0)
+	lcd.write_string('   (------)   ')
+	time.sleep(st)
+	lcd.clear()
+	# frame 7 (3, 1)
+	lcd.write_string('      **      ')
+	lcd.cursor_pos = (1,0)
+	lcd.write_string('  (--------)  ')
+	time.sleep(st)
+	lcd.clear()
+	# frame 8 (2)
+	lcd.write_string('      **      ')
+	lcd.cursor_pos = (1,0)
+	lcd.write_string(' (----------) ')
+	time.sleep(st)
+	lcd.clear()
+
+# Goodbye routine
+lcd.write_string('Goodbye!')
+time.sleep(1)
+lcd.clear()
