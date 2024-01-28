aeb4d18a79dabd7b48e14be7e72ae655172461e7
Clement Goubert <goubert.clement@gmail.com>
2024-01-28 14:22:36 -0500

    xp1

--- 02_16X2LCD_basics/lcd_display.py
+++ 02_16X2LCD_basics/lcd_display.py
@@ -1,6 +1,29 @@
 from RPLCD.i2c import CharLCD
+import time
 
 lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1, cols=16, rows=2, dotsize=8)
 lcd.clear()
 
-lcd.write_string('Hello, World!')
+for i in range(20):
+	lcd.write_string('Hello, Clément!')
+	lcd.clear()
+	time.sleep(.1)
+	lcd.write_string('Hello, Ángela!')
+	lcd.clear()
+	time.sleep(.1)
+
+lcd.write_string('Goodbye!')
+time.sleep(1)
+lcd.clear()
+lcd.write_string('3!')
+time.sleep(1)
+lcd.clear()
+lcd.write_string('2!')
+time.sleep(1)
+lcd.clear()
+lcd.write_string('1!')
+time.sleep(1)
+lcd.clear()
+lcd.write_string('POOF!')
+time.sleep(1)
+lcd.clear()
