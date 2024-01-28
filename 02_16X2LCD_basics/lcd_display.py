e1e9c73a842e78bfbc6c8f67878d88f8d5127678
Clement Goubert <goubert.clement@gmail.com>
2024-01-28 14:24:53 -0500

    change sleep time in loop

--- 02_16X2LCD_basics/lcd_display.py
+++ 02_16X2LCD_basics/lcd_display.py
@@ -1,17 +1,20 @@
 from RPLCD.i2c import CharLCD
 import time
 
+# create lcd object with write address.
 lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1, cols=16, rows=2, dotsize=8)
 lcd.clear()
 
+# Loop of messages
 for i in range(20):
 	lcd.write_string('Hello, Clément!')
+	time.sleep(1)
 	lcd.clear()
-	time.sleep(.1)
 	lcd.write_string('Hello, Ángela!')
+	time.sleep(1)
 	lcd.clear()
-	time.sleep(.1)
 
+# Goodbye routine
 lcd.write_string('Goodbye!')
 time.sleep(1)
 lcd.clear()
